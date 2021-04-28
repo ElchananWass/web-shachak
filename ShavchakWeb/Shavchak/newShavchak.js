3f8d248aefbfcdda83ae168b1c15bd0c26e61609
ElchananWass <Elchananwass@hotmail.com>
2021-04-29 00:55:48 +0300

    figured out the yield from cycles

--- ShavchakWeb/Shavchak/newShavchak.js
+++ ShavchakWeb/Shavchak/newShavchak.js
@@ -1,19 +1,37 @@
-function FromCycles() {
-    // get the יחסים from page 
-    //calculate which pazam
-    //and teturns a liat
+cycles = {
 }
 
-function GetSoldiersFor(cyclesList,naglaNum) {
-    // returns soldiers from the cycles
-    // returns a nagla list
+for (i = 1; i <= 7; i++) {
+  name = "pzm_" + i.toString();
+  cycles[name] = [];
+  //alert(cycles[name]);
+}
+
+function* CyclesItirate(i) {
+  // get the יחסים from page 
+  //calculate which pazam
+  //and teturns a liat
+  while (true) {
+    yield i % 7 + 1;
+    i++;
+  }
+}
+const currentCycle = CyclesItirate(0);
+//currentCycle.next().value
+
+function SoldiersForNagla(naglaNum) {
+  // returns soldiers from the cycles
+  // returns a nagla list
+  
+  // draws n soldiers with cycle
+  //"sort them by goodness" and return
 }
 
 function OrderNagla(argument) {
-    // sort by which soldier to which stand
+  // sort by which soldier to which stand
 }
 
 function PushBackFoward(argument) {
-    // a little problamatic but soppuose to change between hours
-    // alot of if else for conditions...
+  // a little problamatic but soppuose to change between hours
+  // alot of if else for conditions...
 }
